883855737cbf78af4d79451ac78f8ed6e180187c
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 22:43:42 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -16,13 +16,13 @@ def first_challenge
   #your code here
   contacts.each do |person, data|
   #at this level, "person" is Jon Snow or Freddy and "data" is a hash of key/value pairs
-  #to iterate over the "data" hash, we can use the following line: 
- 
+  #to iterate over the "data" hash, we can use the following line:
+
   data.each do |attribute, value|
     #at this level, "attribute" describes the key of :name, :email, :favorite_icecream_flavors, or :knows
     #we need to first check and see if the key is :favorite_icecream_flavors,
     #if it is, that means the VALUE is an array that we can iterate over to print out each element
- 
+
     if attribute == :favorite_icecream_flavors
       value.each do |flavor|
         # here, each index element in an ice cream flavor string
@@ -36,4 +36,3 @@ end
   #remember to return your newly altered contacts hash!
   contacts
 end
-
