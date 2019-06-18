ee5be8a80e8968fc57c5994d7e9b5262b89eac70
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 22:44:40 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -27,7 +27,7 @@ def first_challenge
       value.each do |flavor|
         # here, each index element in an ice cream flavor string
         if flavor == "strawberry"
-          favorite_icecream_flavors.delete("strawberry")
+          :favorite_icecream_flavors.delete("strawberry")
         end
       end
     end
